448ee15c3564910a1728963b42e2de804ceb1eef
MatHoyer <mathieuhoyer@gmail.com>
2025-08-05 18:39:11 +0200

    feat: cors + api logger

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,8 @@
 import { serve } from "@hono/node-server";
 import { getUrl } from "@hypertube/libs";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
 import { env } from "./env.js";
 import testRouter from "./routes/test/test.route.js";
 
@@ -8,6 +10,7 @@ const app = new Hono();
 
 app.get(getUrl("api-health"), (c) => c.text("OK"));
 
+app.use(logger(), cors());
 app.route("/api/test", testRouter);
 
 console.log(env);
