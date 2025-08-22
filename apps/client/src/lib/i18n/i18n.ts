357bf6db742e600a6d733b611e19c4e0286a9b7f
Mathieu <76448795+MatHoyer@users.noreply.github.com>
2025-08-22 19:52:55 +0200

    feat: zod default error message locales (#11)

--- apps/client/src/lib/i18n/i18n.ts
+++ apps/client/src/lib/i18n/i18n.ts
@@ -1,6 +1,7 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import z from "zod";
 import en from "./en.json";
 import fr from "./fr.json";
 
@@ -17,4 +18,13 @@ i18n.use(initReactI18next).use(LanguageDetector).init({
   lng: "en",
   fallbackLng: "en",
   resources,
+});
+
+const zodLocales = {
+  en: z.locales.en(),
+  fr: z.locales.fr(),
+};
+
+i18n.on("languageChanged", (lng) => {
+  z.config(zodLocales[lng as keyof typeof zodLocales]);
 });
