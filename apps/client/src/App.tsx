3ca95ed4c0bf4e0fff7242ab3ac62ba69f33a198
MatHoyer <mathieuhoyer@gmail.com>
2025-08-23 09:36:50 +0200

    feat: add the type for the isLogged context

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,12 +1,13 @@
-import { SignInForm } from "./components/SignInForm";
+import { LanguageSwitcher } from "./components/LanguageSwitcher";
 import { SignOutButton } from "./components/LogOutButton";
+import { SignInForm } from "./components/SignInForm";
 import { SignUpForm } from "./components/SignUpForm";
 import { authClient } from "./lib/auth-client";
-import { LanguageSwitcher } from "./components/LanguageSwitcher";
 
 const App = () => {
   const user = authClient.useSession();
   console.log(user);
+
   return (
     <div className="size-full flex justify-center items-center">
       <LanguageSwitcher />
