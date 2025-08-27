cbcc8da36cabb76daf3ecf21b910a0de464dbe62
elodie <elodie15@gmail.com>
2025-08-27 14:47:00 +0200

    feat: create Protected Route to check for uuid URL param

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,11 +2,12 @@ import { NotFoundPage } from "@/pages/notFound/NotFound.page";
 import { SignInPage } from "@/pages/signIn/SignIn.page";
 import { SignUpPage } from "@/pages/signUp/SignUp.page";
 import { getUrl } from "@hypertube/libs";
-import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+import { Navigate, Outlet, Route, Routes, useParams } from "react-router-dom";
 import { PrivateLayout } from "./layouts/PrivateLayout";
 import { PublicLayout } from "./layouts/PublicLayout";
 import { authClient } from "./lib/auth-client";
 import MoviePage from "./pages/movie/movie.page";
+import { z } from "zod";
 
 const PublicRoute = () => {
   const session = authClient.useSession();
@@ -32,6 +33,20 @@ const PrivateRoute = () => {
   );
 };
 
+const ProtectedRoute = ({ param }: { param: string }) => {
+  const params = useParams();
+  const uuidSchema = z.string().uuid();
+
+  const paramValue = params[param];
+  const isValidUuid = uuidSchema.safeParse(paramValue).success;
+
+  if (!isValidUuid) {
+    return <NotFoundPage />;
+  }
+
+  return <Outlet />
+}
+
 const App = () => {
   const session = authClient.useSession();
   console.log(session?.data?.user);
@@ -43,12 +58,14 @@ const App = () => {
         <Route element={<PublicRoute />}>
           <Route path={getUrl("client-signin")} element={<SignInPage />} />
           <Route path={getUrl("client-signup")} element={<SignUpPage />} />
-          <Route
-            path={getUrl("client-movie", {
-              movieId: ":movieId",
-            })}
-            element={<MoviePage />}
-          />
+          <Route element={<ProtectedRoute param="movieId" />}>
+            <Route
+              path={getUrl("client-movie", {
+                movieId: ":movieId",
+              })}
+              element={<MoviePage />}
+            />
+          </Route>
         </Route>
 
         {/* Routes privées */}
