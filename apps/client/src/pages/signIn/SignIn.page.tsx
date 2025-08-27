ff6a72fb1bbd868fc98e2ed12d44794173b245e9
MatHoyer <mathieuhoyer@gmail.com>
2025-08-27 20:57:17 +0200

    test

--- apps/client/src/pages/signIn/SignIn.page.tsx
+++ apps/client/src/pages/signIn/SignIn.page.tsx
@@ -1,6 +1,9 @@
+import { useTranslation } from "react-i18next";
 import { SignInForm } from "./components/SignInForm";
 
 export const SignInPage = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="size-full">
       <SignInForm />
