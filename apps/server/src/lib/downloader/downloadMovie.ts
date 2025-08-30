8fa307f3902eaede7e7467228b5fac378d2d6d05
MatHoyer <mathieuhoyer@gmail.com>
2025-08-30 10:43:51 +0200

    fix: better downloader import

--- apps/server/src/lib/downloader/downloadMovie.ts
+++ apps/server/src/lib/downloader/downloadMovie.ts
@@ -1,7 +1,7 @@
 import type { TMovieSchema } from "@hypertube/libs";
 import { TResolutionSchema } from "@hypertube/libs";
-import { env } from "../../env";
 import { getResolutionPath } from "../movie-folder-gestion/resolution";
+import { downloader } from "./downloader";
 
 export const downloadMovie = async (
   movieId: TMovieSchema["id"],
@@ -9,20 +9,14 @@ export const downloadMovie = async (
 ) => {
   const resolutionPath = getResolutionPath(movieId, resolution, true).slice(1);
 
-  const Transmission = (await import("transmission-promise")).default;
-  const transmission = new Transmission({
-    host: env.TRANSMISSION_HOST,
-    port: env.TRANSMISSION_RCP_PORT,
-  });
-
   console.log("Downloading movie", resolutionPath);
 
   try {
-    const result = await transmission.addFile(resolutionPath, {
+    const result = await downloader.addFile(resolutionPath, {
       "download-dir": "/downloads",
     });
-    console.log("Torrent ajouté avec l’ID:", result.id);
+    console.log("Torrent added with ID:", result.id);
   } catch (err) {
-    console.error("Erreur lors de l’ajout du torrent:", err);
+    console.error("Error adding torrent:", err);
   }
 };
