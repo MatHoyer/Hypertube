e1ea56ad88bd12b64c99383f3edf42b9cae6f02b
Mathieu <76448795+MatHoyer@users.noreply.github.com>
2025-09-27 18:58:40 +0200

    fix: page layout used in pages not global (#67)

--- apps/client/src/components/UserDropdown.tsx
+++ apps/client/src/components/UserDropdown.tsx
@@ -13,13 +13,12 @@ import { getUrl } from "@hypertube/libs";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { File, LogOut, Settings, User } from "lucide-react";
 import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { UserImageAvatar } from "./images/Avatar";
 
 export const UserDropdown = () => {
   const queryClient = useQueryClient();
   const { t } = useTranslation();
-  const navigate = useNavigate();
 
   const signOutMutation = useMutation({
     mutationFn: async () => {
@@ -42,24 +41,25 @@ export const UserDropdown = () => {
       <DropdownMenuContent className="w-56" align="end">
         <DropdownMenuLabel>{t("navbar.account")}</DropdownMenuLabel>
         <DropdownMenuGroup>
-          <DropdownMenuItem onClick={() => navigate("#")}>
-            <User /> {t("navbar.profile")}
+          <DropdownMenuItem asChild>
+            <Link to={"#"}>
+              <User /> {t("navbar.profile")}
+            </Link>
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => navigate(getUrl("client-settings"))}>
-            <Settings /> {t("navbar.settings")}
+          <DropdownMenuItem asChild>
+            <Link to={getUrl("client-settings")}>
+              <Settings /> {t("navbar.settings")}
+            </Link>
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem
-          onClick={() =>
-            // Do not use navigate because we need to reload the page to see the swagger UI
-            (document.location.href = getUrl("api-swagger", {
-              mode: "ui",
-              withServerUrl: true,
-            }))
-          }
-        >
-          <File /> API
+        <DropdownMenuItem asChild>
+          <Link
+            to={getUrl("api-swagger", { mode: "ui", withServerUrl: true })}
+            target="_blank"
+          >
+            <File /> API
+          </Link>
         </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem
