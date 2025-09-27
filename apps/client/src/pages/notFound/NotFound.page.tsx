d5d5eb4c584693ddf78ebda3f6fab1d2421e3f80
NicolasR <nicolas.romanoff94@gmail.com>
2025-09-27 17:56:54 +0200

    feat: Page 404

--- apps/client/src/pages/notFound/NotFound.page.tsx
+++ apps/client/src/pages/notFound/NotFound.page.tsx
@@ -1,7 +1,34 @@
+import { Typography } from "@/components/ui/typography";
+import { useTranslation } from "react-i18next";
+
 export const NotFoundPage = () => {
+  const { t } = useTranslation();
+  const len = 50;
   return (
-     <div className="size-full">
-      <h1 className="text-6xl font-bold">404</h1>
+    <div className="relative flex flex-col justify-center items-center h-[calc(100dvh-65px)] overflow-hidden">
+      <div
+        className="absolute size-52 rounded-full text-center content-center select-none bg-primary/60 backdrop-blur-sm"
+        onClick={() => {
+          console.log("go back to homepage");
+        }}
+      >
+        <Typography variant="h1">404</Typography>
+        <Typography variant="h3">{t("global.homepage")}</Typography>
+      </div>
+      <img
+        src="/images/404/404top.svg"
+        className="select-none"
+        draggable={false}
+        width={len}
+        height={len}
+      />
+      <img
+        src="/images/404/404bottom.svg"
+        className="select-none"
+        draggable={false}
+        width={len}
+        height={len}
+      />
     </div>
   );
 };
