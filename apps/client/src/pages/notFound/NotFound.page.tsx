6206f9726a4a70adaa65861ddb1d3514fc8b606e
Nicolas R. <133759127+NicolasRomanoff@users.noreply.github.com>
2025-09-29 21:03:10 +0200

    feat: Page 404 with animation (#66)
    * feat: Page 404
    
    * feat: animation

--- apps/client/src/pages/notFound/NotFound.page.tsx
+++ apps/client/src/pages/notFound/NotFound.page.tsx
@@ -1,7 +1,51 @@
+import { Typography } from "@/components/ui/typography";
+import { useMouse } from "@/hooks/use-mouse";
+import { cn } from "@/lib/utils";
+import { getUrl } from "@hypertube/libs";
+import { useRef } from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+
 export const NotFoundPage = () => {
+  const { t } = useTranslation();
+  const ref = useRef(null);
+  const { mouseIn } = useMouse(ref);
+  const navigate = useNavigate();
+
   return (
-     <div className="size-full">
-      <h1 className="text-6xl font-bold">404</h1>
+    <div className="relative h-[calc(100dvh-65px)] overflow-hidden">
+      <div className="absolute flex justify-center items-center inset-0">
+        <div
+          className="z-10 size-52 rounded-full text-center content-center select-none bg-primary/60 backdrop-blur-sm"
+          ref={ref}
+          onClick={() => navigate(getUrl("client-home"))}
+        >
+          <Typography variant="h1">404</Typography>
+          <Typography variant="h3">{t("global.homepage")}</Typography>
+        </div>
+      </div>
+      <div
+        className={cn(
+          "flex flex-col size-full justify-center items-center transition-all animate-spin",
+          mouseIn ? "gap-0" : "gap-60"
+        )}
+        style={{ animationDuration: "15s" }}
+      >
+        <img
+          src="/images/404/404top.svg"
+          className="select-none"
+          draggable={false}
+          width={50}
+          height={50}
+        />
+        <img
+          src="/images/404/404bottom.svg"
+          className="select-none"
+          draggable={false}
+          width={50}
+          height={50}
+        />
+      </div>
     </div>
   );
 };
