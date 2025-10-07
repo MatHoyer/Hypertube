b23b2002758173596853037659d496af020d7c9a
NicolasR <nicolas.romanoff94@gmail.com>
2025-10-07 18:13:20 +0200

    revert: override env

--- apps/server/src/env.ts
+++ apps/server/src/env.ts
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 import z from "zod";
 
-dotenv.config({ path: "../../.env", override: true });
+dotenv.config({ path: "../../.env" });
 
 const envSchema = z.object({
   POSTGRES_HOST: z.string(),
