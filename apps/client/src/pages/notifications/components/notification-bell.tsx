3b3140d94e75f69fa8cf36b80521b4b3e82a7af3
MatHoyer <mathieuhoyer@gmail.com>
2025-10-31 17:02:29 +0100

    fix: new notifications stats hook

--- apps/client/src/pages/notifications/components/notification-bell.tsx
+++ apps/client/src/pages/notifications/components/notification-bell.tsx
@@ -1,28 +1,12 @@
-import { axiosFetch } from "@/lib/fetch/axiosFetch";
+import { useNotificationsStats } from "@/hooks/use-notifications-stats";
 import { cn } from "@/lib/utils";
-import { getNotificationsStatsSchemas, getUrl } from "@hypertube/libs";
-import { useQuery } from "@tanstack/react-query";
 import { Bell } from "lucide-react";
 import type React from "react";
-import { useMemo } from "react";
 
 export const NotificationBell: React.FC<{ size?: 20 | 42 }> = ({
   size = 20,
 }) => {
-  const { data: stats } = useQuery({
-    queryKey: ["notifications", "stats"],
-    queryFn: async () => {
-      return axiosFetch({
-        method: "GET",
-        url: getUrl("api-notifications-stats"),
-        schemas: getNotificationsStatsSchemas,
-      });
-    },
-  });
-
-  const isUnreadNotifications = useMemo(() => {
-    return stats?.totalUnreadNotifications ?? 0 > 0;
-  }, [stats?.totalUnreadNotifications]);
+  const { isUnreadNotifications } = useNotificationsStats();
 
   return (
     <div className="relative">
@@ -34,8 +18,8 @@ export const NotificationBell: React.FC<{ size?: 20 | 42 }> = ({
         <div
           className={cn(
             "absolute bg-red-500 rounded-full",
-            size === 20 ? "size-2" : "size-6",
-            size === 20 ? "-top-0.5 -right-0.5" : "-top-1 -right-1"
+            size === 20 ? "size-2" : "size-4",
+            size === 20 ? "-top-0.5 -right-0.5" : "top-0 right-0"
           )}
         />
       )}
