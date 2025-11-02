52326a68291ec9a1186f89c96574cf64eabb4cfe
MatHoyer <mathieuhoyer@gmail.com>
2025-11-02 10:58:01 +0100

    feat: movie routes

--- apps/server/src/routes/movies/movies.controller.ts
+++ apps/server/src/routes/movies/movies.controller.ts
@@ -4,12 +4,14 @@ import {
   getMoviesSchemas,
   hypertubeLogger,
   languageCodes,
+  TDeleteMovieSubscribeSchemas,
   TGetMovieSchemas,
   TGetMoviesSchemas,
   TGetMovieSSESchemas,
   tmdbMovieSchema,
   TPostMovieDownloadResolutionSchemas,
   TPostMovieDownloadSubtitlesSchemas,
+  TPostMovieSubscribeSchemas,
 } from "@hypertube/libs";
 import { env, prisma } from "@hypertube/server-core";
 import { Context } from "hono";
@@ -20,6 +22,7 @@ import { downloadTorrent } from "../../lib/downloader/downloadTorrent";
 import { downloaderQueue } from "../../lib/queues/downloader";
 import { downloadYifysubtitles } from "../../lib/scrappers/yifysubtitles.scrapper";
 import { SSEClients } from "../../lib/SSEClients";
+import { TIsLogged } from "../../middlewares/isLogged";
 import { TIsLoggedSafe } from "../../middlewares/isLoggedSafe";
 import { TSearchParamsParser } from "../../middlewares/searchParamsParser";
 import { TUrlParamsParser } from "../../middlewares/urlParamsParser";
@@ -301,4 +304,60 @@ export const downloadSubtitles = async (
   });
 
   return c.json({ message: "Subtitles downloaded" });
+};
+
+export const subscribeToMovie = async (
+  c: Context<
+    TIsLogged & TUrlParamsParser<TPostMovieSubscribeSchemas["urlParams"]>
+  >
+) => {
+  const { tmdbId } = c.get("validatedUrlParams");
+  const { id: userId } = c.get("user");
+
+  const dbMovie = await prisma.movie.findUnique({
+    where: {
+      tmdbId,
+    },
+  });
+  if (!dbMovie) {
+    return c.json({ message: "Movie not found" }, 404);
+  }
+
+  await prisma.movieSubscription.create({
+    data: {
+      movieId: dbMovie.id,
+      userId,
+    },
+  });
+
+  return c.json({ message: "Movie subscribed" });
+};
+
+export const unsubscribeFromMovie = async (
+  c: Context<
+    TIsLogged & TUrlParamsParser<TDeleteMovieSubscribeSchemas["urlParams"]>
+  >
+) => {
+  const { tmdbId } = c.get("validatedUrlParams");
+  const { id: userId } = c.get("user");
+
+  const dbMovie = await prisma.movie.findUnique({
+    where: {
+      tmdbId,
+    },
+  });
+  if (!dbMovie) {
+    return c.json({ message: "Movie not found" }, 404);
+  }
+
+  await prisma.movieSubscription.delete({
+    where: {
+      movieId_userId: {
+        movieId: dbMovie.id,
+        userId,
+      },
+    },
+  });
+
+  return c.json({ message: "Movie unsubscribed" });
 };
