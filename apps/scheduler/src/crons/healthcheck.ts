8c630c9b9ee8890796e79c7c0756becb1eb388f5
MatHoyer <mathieuhoyer@gmail.com>
2025-11-02 10:59:00 +0100

    fix: remove notif schedule

--- apps/scheduler/src/crons/healthcheck.ts
+++ apps/scheduler/src/crons/healthcheck.ts
@@ -1,5 +1,4 @@
 import { TLogger } from "@hypertube/libs";
-import { generateNotification, notifications } from "@hypertube/server-core";
 import { cronUTC } from "../cronUTC.js";
 
 const HEALTHCHECK_CRON_EXPRESSION = "0 */1 * * * *";
@@ -7,9 +6,6 @@ const HEALTHCHECK_CRON_NAME = "Healthcheck";
 
 const HEALTHCHECK_CRON_CALLBACK = async (localLogger: TLogger) => {
   localLogger.info("Scheduler healthcheck");
-  generateNotification("cb3lmsYyZftpGrmHYVGtOBtUMB6Jetom", notifications.TEST, {
-    someId: 1,
-  });
 };
 
 export const healthcheckCron = () => {
