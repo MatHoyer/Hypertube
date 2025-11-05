b5d20f1d841930fa65da800da9d2703ad288113d
NicolasR <nicolas.romanoff94@gmail.com>
2025-11-05 15:23:44 +0100

    feat: MovieBaseInfo => Thumbnail dialog's & movie page

---
+++ apps/client/src/components/movies/MovieBaseInfo.tsx
@@ -0,0 +1,51 @@
+import { cn } from "@/lib/utils";
+import type { TGetMovieSchemas, TTmdbMovieSchema } from "@hypertube/libs";
+import { useTranslation } from "react-i18next";
+import { ImageContainer } from "../images/ImageContainer";
+import { Badge } from "../ui/badge";
+import { Typography } from "../ui/typography";
+import { ScoreRated } from "./ScoreRated";
+
+type TMovieBaseInfo = Omit<TTmdbMovieSchema, "id"> & {
+  id: TTmdbMovieSchema["id"] | TGetMovieSchemas["response"]["id"];
+};
+
+export const MovieBaseInfo: React.FC<{
+  movie: TMovieBaseInfo;
+  truncate?: boolean;
+}> = ({ movie, truncate = false }) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="flex flex-col items-center w-full gap-2 text-center">
+      <ImageContainer
+        imageSrc={movie.poster_path}
+        altImage={movie.title}
+        size={truncate ? "md" : "lg"}
+      />
+      <Typography variant="h1">{movie.title}</Typography>
+      <div className="flex gap-2">
+        <Typography variant="muted" className={cn(truncate && "line-clamp-1")}>
+          {movie.original_title}
+        </Typography>
+        <Badge variant={"outline"}>
+          {movie.original_language.toUpperCase()}
+        </Badge>
+      </div>
+      <Typography className={cn(truncate && "line-clamp-5")}>
+        {movie.overview || t("movie.page.missing.desc")}
+      </Typography>
+      <div className="flex gap-2 justify-center">
+        {movie.genres.map(({ name }, i) => (
+          <Badge key={i} variant={"outline"}>
+            {name}
+          </Badge>
+        ))}
+      </div>
+      <div className="flex justify-between w-full">
+        <Badge>{movie.release_date}</Badge>
+        <ScoreRated score={movie.vote_average} />
+      </div>
+    </div>
+  );
+};
--- apps/client/src/pages/library/dialogs/movie-dialog.tsx
+++ apps/client/src/pages/library/dialogs/movie-dialog.tsx
@@ -1,59 +1,27 @@
 import { closeDialog } from "@/components/dialogs/dialog.store";
-import { ImageContainer } from "@/components/images/ImageContainer";
+import { MovieBaseInfo } from "@/components/movies/MovieBaseInfo";
 import { Button } from "@/components/ui/button";
-import {
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
+import { DialogContent, DialogFooter } from "@/components/ui/dialog";
 import { getUrl, type TTmdbMovieSchema } from "@hypertube/libs";
-import { PlayCircle, Star } from "lucide-react";
-import { useTranslation } from "react-i18next";
+import { PlayCircle } from "lucide-react";
 import { Link } from "react-router-dom";
 
 export const MovieDialog: React.FC<{ movie: TTmdbMovieSchema }> = ({
   movie,
 }) => {
-  const { t } = useTranslation();
-
   return (
     <DialogContent className="flex flex-col items-center">
-      <ImageContainer
-        imageSrc={movie.poster_path}
-        altImage={movie.title}
-        size="md"
-      />
-      <div className="w-full">
-        <DialogHeader>
-          <DialogTitle>{movie.title}</DialogTitle>
-          <DialogDescription>
-            {movie.overview || t("movie.page.missing.desc")}
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid grid-cols-3 m-2">
-          <div className="col-span-2">
-            {movie.genres.length
-              ? movie.genres.map(({ name }) => name).join(" / ")
-              : t("movie.page.missing.genres")}
-          </div>
-          <div className="flex items-center justify-end gap-1">
-            {movie.vote_average.toPrecision(2)}/10
-            <Star fill="yellow" color="black" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button variant={"outline"} className="w-full" asChild>
-            <Link
-              to={getUrl("client-movie", { tmdbId: movie.id })}
-              onClick={() => closeDialog()}
-            >
-              <PlayCircle />
-            </Link>
-          </Button>
-        </DialogFooter>
-      </div>
+      <MovieBaseInfo movie={movie} truncate />
+      <DialogFooter className="w-full">
+        <Button variant={"outline"} className="w-full" asChild>
+          <Link
+            to={getUrl("client-movie", { tmdbId: movie.id })}
+            onClick={() => closeDialog()}
+          >
+            <PlayCircle />
+          </Link>
+        </Button>
+      </DialogFooter>
     </DialogContent>
   );
 };
