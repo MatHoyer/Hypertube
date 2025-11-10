e07c2375268163a8a0a20b97508b8178837d781d
NicolasR <nicolas.romanoff94@gmail.com>
2025-11-10 16:06:32 +0100

    feat: status / watch + dropdown for future features

--- apps/client/src/components/movies/ScoreRated.tsx
+++ apps/client/src/components/movies/ScoreRated.tsx
@@ -6,11 +6,11 @@ export const ScoreRated: React.FC<
   ComponentProps<"div"> & { score: number; total?: number }
 > = ({ score, total = 10 }, ...props) => {
   return (
-    <div {...props} className="flex">
+    <div {...props} className="flex items-center">
       <Typography variant="mono">
         {score.toPrecision(2)}/{total}
       </Typography>
-      <Star color="black" fill="yellow" />
+      <Star color="black" fill="yellow" size={15} />
     </div>
   );
 };
--- apps/client/src/pages/library/components/Thumbnail.tsx
+++ apps/client/src/pages/library/components/Thumbnail.tsx
@@ -1,15 +1,52 @@
 import { openDialog } from "@/components/dialogs/dialog.store";
 import { MovieBaseInfo } from "@/components/movies/MovieBaseInfo";
+import { Badge } from "@/components/ui/badge";
 import { Card } from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import type { TTmdbMovieSchema } from "@hypertube/libs";
+import { Check, EllipsisVertical, Info } from "lucide-react";
 
 export const Thumbnail: React.FC<{ movie: TTmdbMovieSchema }> = ({ movie }) => {
   return (
-    <Card
-      className="flex p-2 md:p-4 items-center"
-      onClick={() => openDialog("movie", movie)}
-    >
-      <MovieBaseInfo movie={movie} posterSize="md" info="partial" />
-    </Card>
+    <div>
+      <Card
+        className="flex gap-0 p-2 md:p-4 items-center rounded-b-none"
+        onClick={() => openDialog("movie", movie)}
+      >
+        <MovieBaseInfo movie={movie} posterSize="md" info="partial" />
+      </Card>
+      <Card className="p-2 rounded-t-none border-t-0">
+        <div className="flex gap-2 w-full items-center justify-between">
+          <div className="overflow-hidden">
+            <Badge variant={"outline"}>
+              <Info />
+              Status
+            </Badge>
+          </div>
+          <div className="flex items-center gap-2">
+            <Badge variant={"success"}>
+              <Check />
+            </Badge>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <EllipsisVertical size={15} />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>Default playlist</DropdownMenuItem>
+                  <DropdownMenuItem>Choose playlist</DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </Card>
+    </div>
   );
 };
