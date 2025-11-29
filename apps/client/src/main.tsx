f603cc6c0cc499379ab202168f0e5abf49a8ff70
MatHoyer <mathieuhoyer@gmail.com>
2025-11-29 09:38:22 +0100

    fix: set default theme to dark

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -23,7 +23,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <NuqsAdapter>
-          <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+          <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
             <App />
             <GlobalDialog />
             <GlobalAlertDialog />
