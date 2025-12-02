768a2bd35f6873393fd85a3bfaad09ea30d4c7b3
Nicolas R. <133759127+NicolasRomanoff@users.noreply.github.com>
2025-12-02 20:34:59 +0100

    Apply suggestion from @NicolasRomanoff

--- apps/client/src/components/ErrorPage.tsx
+++ apps/client/src/components/ErrorPage.tsx
@@ -15,7 +15,6 @@ export const ErrorPage: React.FC<{
 
   return (
     <div className="size-full flex flex-col justify-center items-center gap-4">
-      <AppLoader size={60} />
       <Typography variant="h3">
         {t("global.errorMessage", {
           resource: t(`${resource}.errorRessource`),
