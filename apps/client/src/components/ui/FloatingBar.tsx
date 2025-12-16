d98cdb69388cd5cf096e6a9fc6822c4f8eb1fcb1
NicolasR <nicolas.romanoff94@gmail.com>
2025-12-16 16:57:50 +0100

    fix: scroll on floating bar div

--- apps/client/src/components/ui/FloatingBar.tsx
+++ apps/client/src/components/ui/FloatingBar.tsx
@@ -7,11 +7,13 @@ export const FloatingBar: React.FC<ComponentProps<"div">> = ({
   ...props
 }) => {
   return (
-    <div className={cn("fixed w-full z-10 bottom-0 right-0")}>
+    <div
+      className={cn("fixed w-full z-10 bottom-0 right-0 pointer-events-none")}
+    >
       <div className={cn("flex w-full p-4 justify-center relative")}>
         <div
           className={cn(
-            "flex justify-center rounded-lg bg-muted border border-card p-1",
+            "flex justify-center rounded-lg bg-muted border border-card p-1 pointer-events-auto",
             className
           )}
           {...props}
