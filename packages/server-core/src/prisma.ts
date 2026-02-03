25dbc3745f8107e42f42c69d6b93aea3a0e6341f
NicolasR <nicolas.romanoff94@gmail.com>
2026-02-03 10:17:45 +0100

    feat: PrismaPg schema comment

--- packages/server-core/src/prisma.ts
+++ packages/server-core/src/prisma.ts
@@ -1,6 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
+// Default schema for PrismaPg is "public"
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
