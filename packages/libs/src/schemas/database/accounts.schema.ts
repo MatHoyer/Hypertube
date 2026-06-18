5baa71e56200b5aff17cd550c73a0bb25b3b3abe
NicolasR <nicolas.romanoff94@gmail.com>
2026-06-18 23:02:18 +0200

    fix: disable linking with other emails

--- packages/libs/src/schemas/database/accounts.schema.ts
+++ packages/libs/src/schemas/database/accounts.schema.ts
@@ -4,8 +4,6 @@ export const accountsSchema = z.array(
   z.object({
     id: z.string(),
     providerId: z.string(),
-    provider: z.string(),
-    providerEmail: z.string().nullable(),
     createdAt: z.coerce.date(),
     updatedAt: z.coerce.date(),
     accountId: z.string(),
