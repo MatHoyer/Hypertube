2da0dbe1bc99363649fb870eece4ddaca64f64cc
NicolasR <nicolas.romanoff94@gmail.com>
2026-06-19 23:12:57 +0200

    feat: index for services export

--- packages/server-core/src/index.ts
+++ packages/server-core/src/index.ts
@@ -3,5 +3,6 @@ export * from "./files/index.js";
 export * from "./notifications/index.js";
 export * from "./prisma.js";
 export * from "./redis/index.js";
+export * from "./services/index.js";
 export * from "./storage/index.js";
 export * from "./utils/index.js";
