32a0b63a5fea3b1a08f3c575eaad6de36fe0a432
NicolasR <nicolas.romanoff94@gmail.com>
2026-06-19 04:17:02 +0200

    refacto: enum for providerId for accounts

--- packages/libs/src/schemas/database/accounts.schema.ts
+++ packages/libs/src/schemas/database/accounts.schema.ts
@@ -1,9 +1,10 @@
 import z from "zod";
+import { betterAuthProviders, credentialId } from "../../const/global.const.js";
 
 export const accountsSchema = z.array(
   z.object({
     id: z.string(),
-    providerId: z.string(),
+    providerId: z.enum([...betterAuthProviders, credentialId]),
     createdAt: z.coerce.date(),
     updatedAt: z.coerce.date(),
     accountId: z.string(),
